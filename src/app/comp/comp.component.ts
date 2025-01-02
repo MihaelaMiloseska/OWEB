a770ea9da94ea6339a65d33c5c1eff49d70b2f29
MihaelaMiloseska <mihaelamiloseska16@gmail.com>
2025-01-02 23:21:11 +0100

    Update comp.component.ts

--- src/app/comp/comp.component.ts
+++ src/app/comp/comp.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+/*import { Component, Input } from '@angular/core';
 import { driver } from '../driver';
 
 
@@ -16,4 +16,64 @@ onDrvView() {
  vozac:driver | undefined;
  @Input() 
  indeks:number|undefined;
+}
+*/
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { driver } from '../driver';
+
+@Component({
+  selector: 'app-comp',
+  standalone: true, // Standalone komponenta
+  imports: [CommonModule], // Dodavanje na CommonModule
+  
+  templateUrl: './comp.component.html',
+
+  styleUrls: ['./comp.component.css']
+})
+export class CompComponent {
+
+  
+  @Input()
+  vozac: driver | undefined;
+
+  @Input()
+  indeks: number | undefined;
+  onDrvView(){
+  
+    let link: string |undefined; 
+  
+    if(this.vozac?.iconUrl)
+    {link=this.vozac?.iconUrl}
+    else{
+      link="https://www.google.com/"
+    };
+    window.open(link,"_blank")
+  }
+
+ 
+
+  klasi() {
+    return {
+      'begin': this.vozac?.category == 'ASD',
+      'adv': this.vozac?.category == 'EXPERT',
+      'undr': true
+    };
+  }
+
+  klasi2() {
+    if (this.vozac?.category == 'ASD') {
+      return 'begin';
+    } else {
+      return 'adv';
+    }
+  }
+
+  stilovi() {
+    if (this.vozac?.category == 'EXPERT') {
+      return 'underline';
+    } else {
+      return 'none';
+    }
+  }
 }
